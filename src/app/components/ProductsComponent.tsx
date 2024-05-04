d70c30f9813534cab331ef78dd6a4ad51312d326
itzteeler <tylernguyen518@gmail.com>
2024-05-04 10:50:13 -0700

    Fonts

--- src/app/components/ProductsComponent.tsx
+++ src/app/components/ProductsComponent.tsx
@@ -4,9 +4,9 @@ const ProductsComponent = () => {
     return (
         <div className='bg-[#96D4EC] grid grid-cols-2 p-[50px]'>
             <div className='border-r border-black pb-[50px] px-[50px]'>
-                <p className='text-[2.5rem] font-bold'>Know the Dangers</p>
+                <p className='text-[2.5rem] font-[PoppinsBold]'>Know the Dangers</p>
             </div>
-            <div className='p-[50px]'>
+            <div className='p-[50px] font-[PoppinsLight]'>
                 <p>{"On average, more deaths occur due to flooding each year than from any other severe weather related hazard."}</p>
                 <p>{"Nearly half of all flood fatalities occur in vehicles."}</p>
                 <p>{'Those living outside of mapped "high-risk" flood areas file nearly 25% of all flood insurance claims and receive 1/3rd of Federal Disaster Assistance aid.'}</p>
