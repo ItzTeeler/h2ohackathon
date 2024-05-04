55a08de22b85adc2cef752a188ca9717dc1ca1ac
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-05-04 10:25:55 -0700

    change

--- src/app/components/HeaderComponent.tsx
+++ src/app/components/HeaderComponent.tsx
@@ -6,9 +6,8 @@ const HeaderComponent = () => {
 
     return (
         <div style={{
-            backgroundImage: "rgb(29,31,33)",
-            background: "linear-gradient(180deg, rgba(29,31,33,1) 0%, rgba(57,62,68,1) 73%)"
-        }} className='h-full w-screen  overflow-hidden' >
+
+        }} className='h-full w-screen bg-[#1d1f21]  overflow-hidden' >
             <div className='grid grid-cols-2 h-[500px] ml-[100px]'>
                 <div className='col-span-1 my-auto'>
                     <p className='text-[54px] text-white font-semibold'>Understanding Flood Risks,</p>
