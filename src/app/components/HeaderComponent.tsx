b230b7221cec4a35f48c48c16afd57f19d7f2d4f
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-05-04 10:19:08 -0700

    marcos

--- src/app/components/HeaderComponent.tsx
+++ src/app/components/HeaderComponent.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import headerImg from '@/assets/headerImg.png'
+import Image from 'next/image'
 
 const HeaderComponent = () => {
 
     return (
-        <div>
-
+        <div style={{
+            backgroundImage: "rgb(29,31,33)",
+            background: "linear-gradient(180deg, rgba(29,31,33,1) 0%, rgba(57,62,68,1) 73%)"
+        }} className='h-full w-screen  overflow-hidden' >
+            <div className='grid grid-cols-2 h-[500px] ml-[100px]'>
+                <div className='col-span-1 my-auto'>
+                    <p className='text-[54px] text-white font-semibold'>Understanding Flood Risks,</p>
+                    <p className='text-[20px] text-white font-thin '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, impedit quam ratione explicabo totam maxime distinctio atque, voluptate voluptatem cupiditate beatae dignissimos, neque aliquid nesciunt eos voluptatibus officia maiores debitis?</p>
+                </div>
+                <div className='col-span-1 relative'>
+                    <Image className=' h-auto w-[500px] absolute bottom-0 right-0 object-bottom object-right' src={headerImg} alt="header img" />
+                </div>
+            </div>
         </div>
+
+
     )
 }
 
