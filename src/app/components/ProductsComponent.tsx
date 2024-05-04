18765e1d61bb745b67930ba7be0d78f8609c59c6
itzteeler <tylernguyen518@gmail.com>
2024-05-04 10:30:51 -0700

    {}

--- src/app/components/ProductsComponent.tsx
+++ src/app/components/ProductsComponent.tsx
@@ -7,10 +7,10 @@ const ProductsComponent = () => {
                 <p className='text-[2.5rem] font-bold'>Know the Dangers</p>
             </div>
             <div className='p-[50px]'>
-                <p>On average, more deaths occur due to flooding each year than from any other severe weather related hazard.</p>
-                <p>Nearly half of all flood fatalities occur in vehicles.</p>
-                <p>Those living outside of mapped "high-risk" flood areas file nearly 25% of all flood insurance claims and receive 1/3rd of Federal Disaster Assistance aid.</p>
-                <p>An area&quot;s risk of flood can change over time due to new construction, changes in levee classification, or other environmental factors.</p>
+                <p>{"On average, more deaths occur due to flooding each year than from any other severe weather related hazard."}</p>
+                <p>{"Nearly half of all flood fatalities occur in vehicles."}</p>
+                <p>{'Those living outside of mapped "high-risk" flood areas file nearly 25% of all flood insurance claims and receive 1/3rd of Federal Disaster Assistance aid.'}</p>
+                <p>{"An area's risk of flood can change over time due to new construction, changes in levee classification, or other environmental factors."}</p>
 
             </div>
 
