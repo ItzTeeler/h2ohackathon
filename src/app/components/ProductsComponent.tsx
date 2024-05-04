e62c272eddac8aeb55893489dc4a02dd6ae0482d
itzteeler <tylernguyen518@gmail.com>
2024-05-04 13:01:42 -0700

    alt tags

--- src/app/components/ProductsComponent.tsx
+++ src/app/components/ProductsComponent.tsx
@@ -17,7 +17,7 @@ const ProductsComponent = () => {
             <div className='p-[50px] font-[PoppinsLight] flex flex-col gap-y-[20px]'>
                 <div>
                     <div className='flex gap-[10px] items-center'>
-                        <Image className='w-[32px] h-[32px]' src={waterUpArrow} alt='water up arrow' />
+                        <Image className='w-[32px] h-[32px]' src={waterUpArrow} alt='Water Up Arrow Icon' />
                         <p className='font-[PoppinsBold] text-[32px]'>Hazard</p>
                     </div>
                     <p className='px-[50px]'>{"On average, more deaths occur due to flooding each year than from any other severe weather related hazard."}</p>
@@ -25,7 +25,7 @@ const ProductsComponent = () => {
 
                 <div>
                     <div className='flex gap-[10px] items-center'>
-                        <Image className='w-[32px] h-[32px]' src={carIcon} alt='water up arrow' />
+                        <Image className='w-[32px] h-[32px]' src={carIcon} alt='Car Icon' />
                         <p className='font-[PoppinsBold] text-[32px]'>Vehicles</p>
                     </div>
                     <p className='px-[50px]'>{"Nearly half of all flood fatalities occur in vehicles."}</p>                
@@ -33,7 +33,7 @@ const ProductsComponent = () => {
                 
                 <div>
                     <div className='flex gap-[10px] items-center'>
-                        <Image className='w-[32px] h-[32px]' src={highRiskArea} alt='water up arrow' />
+                        <Image className='w-[32px] h-[32px]' src={highRiskArea} alt='High Risk Area Icon' />
                         <p className='font-[PoppinsBold] text-[32px]'>High-Risk</p>
                     </div>
                     <p className='px-[50px]'>{'Those living outside of mapped "high-risk" flood areas file nearly 25% of all flood insurance claims and receive 1/3rd of Federal Disaster Assistance aid.'}</p>
@@ -41,7 +41,7 @@ const ProductsComponent = () => {
 
                 <div>
                     <div className='flex gap-[10px] items-center'>
-                        <Image className='w-[32px] h-[32px]' src={landScape} alt='water up arrow' />
+                        <Image className='w-[32px] h-[32px]' src={landScape} alt='Landscape Changes Icon' />
                         <p className='font-[PoppinsBold] text-[32px]'>Landscape Changes</p>
                     </div>
                     <p className='px-[50px]'>{"An area's risk of flood can change over time due to new construction, changes in levee classification, or other environmental factors."}</p>
