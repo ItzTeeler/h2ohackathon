c887fa014811053fc9a0b17f296c9af11ae6cdd5
brandoughnut <brandonnguyen2867@gmail.com>
2024-05-04 13:22:11 -0700

    changed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <NavbarComponent />
       <HeaderComponent />
       <ProductsComponent />
-      <FAQsComponent/>
       <FaqComponent />
+      <FAQsComponent/>
       <FooterComponent/>
     </div>
 
