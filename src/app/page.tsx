ee8458a87b2f5552931b48130aed5aa7bb5bb042
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-05-04 10:21:16 -0700

    marcos push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import NavbarComponent from "./components/NavbarComponent";
 import ProductsComponent from "./components/ProductsComponent";
+import HeaderComponent from "./components/HeaderComponent";
 
 export default function Home() {
   return (
     <div>
       <NavbarComponent />
-      <ProductsComponent/>
+      <HeaderComponent />
+      <ProductsComponent />
     </div>
 
   );
