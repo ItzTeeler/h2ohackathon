9483254d9ddcdb07b495a6d16a3a469823c537d3
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-05-04 10:41:18 -0700

    changes

--- src/app/components/HeaderComponent.tsx
+++ src/app/components/HeaderComponent.tsx
@@ -11,7 +11,9 @@ const HeaderComponent = () => {
             <div className='grid grid-cols-2 h-[500px] ml-[100px]'>
                 <div className='col-span-1 my-auto'>
                     <p className='text-[54px] text-white font-semibold'>Understanding Flood Risks,</p>
-                    <p className='text-[20px] text-white font-thin '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, impedit quam ratione explicabo totam maxime distinctio atque, voluptate voluptatem cupiditate beatae dignissimos, neque aliquid nesciunt eos voluptatibus officia maiores debitis?</p>
+                    <p className='text-[20px] text-white font-thin '>
+                        From 2003 to 2012, total flood insurace claims average nearly 4 billion dollars. In the past
+                    </p>
                 </div>
                 <div className='col-span-1 relative'>
                     <Image className=' h-auto w-[500px] absolute bottom-0 right-0 object-bottom object-right' src={headerImg} alt="header img" />
